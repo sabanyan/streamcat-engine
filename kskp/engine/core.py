be1dbe88a15f9bd6eedc1e626b54dcec96aa9820
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-09-19 10:29:02 +0900

    Add the dtor() to Command class.

--- kskp/engine/core.py
+++ kskp/engine/core.py
@@ -333,8 +333,11 @@ class Flow(Datum):
         self.lasts_store.save()
         # 配下のflowのdtorも動かす
         for substep in self.substeps:
-            if isinstance(substep.runnable, Flow):
+            from kskp.store import Command
+            if isinstance(substep.runnable, Flow) or isinstance(substep.runnable, Command):
                 substep.runnable.dtor()
+            else:
+                raise Exception('substep.runnableにFlowまたはCommand以外のオブジェクトが格納されています')
 
 class Point:
     """
