dcece7d09640b82b18b920dab3609ea182177250
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-10-29 17:13:19 +0900

    Fix test_main.py

--- kskp/engine/tests/test_main.py
+++ kskp/engine/tests/test_main.py
@@ -14,39 +14,6 @@ from kskp.store import Library, FLOW_PATH, Frame, Command, Port, Datum, STORE_DI
 
 root = Library.load_root()
 
-class Integer(Datum):
-    """
-    テスト用のクラス
-    下記のSquareCommandで使うdatumをラップするためのもの
-    """
-    def __init__(self):
-        super().__init__(None, 'test', None)
-        self._content = None
-
-    def set_content(self, module):
-        self._content = module
-
-    @property
-    def content(self):
-        return self._content
-
-class Square(Command):
-    """
-    与えられた数値を2乗する
-    テストコード内でのみ使用のため、ここに置いておく
-    """
-    def __init__(self):
-        super().__init__()
-        self.i_ports = [Port('i', 'integer')]
-        self.o_ports = [Port('o_sq', 'integer')]
-
-    def run(self, args, inputs):
-        # 厳密にはframeじゃないが、まぁテスト用のコマンドなので
-        # ラップするのはなんでもいいかなと思いframeにした。
-        frame = Integer()
-        frame.set_content([[inputs['i'][0][0] ** 2]])
-        return {self.o_ports[0].name: frame}
-
 class ExecuteTestCase(unittest.TestCase):
     """
     実際のフロー実行のテスト
