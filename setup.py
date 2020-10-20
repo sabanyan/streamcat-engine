b6fb5edf6a30ccb1bf8da631673685375270ab9a
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-20 20:42:25 +0900

    Add setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='kskp.engine',
     packages=['kskp.engine'],
+    description='Core Engine for Flow-base Data Processing',
+    url='https://www.ksk-anl.com/products/kskp',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
     install_requires=[
         # 0.10.1ではインスコ時にpathtoolsが入らない
         'watchdog==0.9.0'
