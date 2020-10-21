14cd27c94f9a7706ef3a8702e1faac4848c72eac
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-21 14:31:55 +0900

    Gave up to update version in setup.py automatically

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup(
     packages=['kskp.engine'],
     description='Core Engine for Flow-base Data Processing',
     url='https://www.ksk-anl.com/products/kskp',
-    version_config={
-        'version_format': '{tag}',
-        'starting_version': '2.0.0'
-    },
-    setup_requires=['better-setuptools-git-version'],
     install_requires=[
         # 0.10.1ではインスコ時にpathtoolsが入らない
         'watchdog==0.9.0'
