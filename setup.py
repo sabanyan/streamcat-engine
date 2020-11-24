b34f5cb37dd4ea026dd5f5c41782c28284841191
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-11-24 23:20:24 +0900

    Clean up code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='kskp.engine',
     packages=['kskp.engine'],
     description='Core Engine for Flow-base Data Processing',
-    url='https://www.ksk-anl.com/products/kskp',
+    url='https://www.kskp.io',
     install_requires=[
         # 0.10.1ではインスコ時にpathtoolsが入らない
         'watchdog==0.9.0'
