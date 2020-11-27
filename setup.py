46ebef8540e43add31b4bfe1e05fa9428f75d68e
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-11-27 18:32:34 +0900

    Engraved Version number

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='kskp.engine',
     packages=['kskp.engine'],
+    version='3.0',
     description='Core Engine for Flow-base Data Processing',
     url='https://www.kskp.io',
     install_requires=[
