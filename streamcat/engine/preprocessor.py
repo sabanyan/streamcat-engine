2fad3f76cffe54005a8fc2d60bfdca69e427eadd
hiroshi80 <it.gumbler@gmail.com>
2022-06-24 14:41:26 +0900

    Added type hints to appenders.py

--- streamcat/engine/preprocessor.py
+++ streamcat/engine/preprocessor.py
@@ -45,7 +45,6 @@ class Preprocessor:
         self._context = Preprocessor.Context(datum_factory, flow, self._activity_data_dest_appender.activity_uuid)
 
         # Appenders
-        # self._folder_data_dest_appender = FolderDataDestAppender(flow, datum_factory, lock_uuid, self._context.start_at)
         self._cache_data_dest_appender = CacheDataDestAppender(flow, datum_factory, lock_uuid, self._context.start_at)
         self._vis_data_dest_appender = VisDataDestAppender()
 
