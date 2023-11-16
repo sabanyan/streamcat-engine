e47d577b9da5c6b77cba7df6f48d335e362e82c9
hiroshi80 <it.gumbler@gmail.com>
2023-11-16 19:49:40 +0900

    Incremented version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='streamcat.engine',
     packages=['streamcat.engine'],
-    version='3.3.3',
+    version='3.4',
     description='Core Engine for Flow-base Data Processing',
     url='https://www.kskp.io',
     install_requires=[
